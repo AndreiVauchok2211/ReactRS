de666d824b83a2fc84eff77c0f2a1768b3974708
AndreiVauchok2211 <vauchokandrei@gmail.com>
2023-10-29 23:46:27 +0300

    refactor: cnange message

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ class App extends React.Component {
         <h2 className="info">
           Hi, still not have worked out how to fulfil all the conditions of the
           task - will try to do during the cross check. I not can say if the
-          functionality will work out in the end. Please check my work last
+          functionality will work out in the end. Please check my work last.
+          P.S.: If you can help me with the work and the course, please write to
+          me in private messages.
         </h2>
         <Form />
         <Field />

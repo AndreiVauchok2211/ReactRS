5486b7e34e1b644bf7c5b729079e462c54f45e85
AndreiVauchok2211 <vauchokandrei@gmail.com>
2023-10-29 23:38:13 +0300

    feat: add message

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ class App extends React.Component {
   render(): React.ReactNode {
     return (
       <div className="container">
+        <h2 className="info">
+          Hi, still haven't worked out how to fulfil all the conditions of the
+          task - will try to do during the cross check. I can't say if the
+          functionality will work out in the end. Please check my work last
+        </h2>
         <Form />
         <Field />
       </div>
